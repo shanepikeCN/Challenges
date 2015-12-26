a0fad12027f09b26c9f5cdb1d05020f563c302b4
U-Shane-PC\Shane <shane.pike@outlook.com>
2015-12-26 15:04:28 +0800

    Completed forum user script

--- Bash/forum.sh
+++ Bash/forum.sh
@@ -6,13 +6,13 @@ while [[ true ]]; do
 	
 	#while [[ -z $forumname || $forumname =~ ^[:blank:] ]]; do
 	read -e -p "Enter your forum name: " forumname
-	[[ -z $forumname || $forumname =~ ^[^a-zA-Z0-9]+ ]] && echo "Invalid forumname format" && continue
+	[[ -z $forumname || $forumname =~ [^a-zA-Z0-9]+ ]] && echo "Invalid forumname format" && continue
 
 	read -e -p "Enter your age: " age
-	[[ -z $age || $age =~ [^0-9]+  || $age -ge 99 || $age == 0 ]] && echo "Invalid age format" && continue
+	[[ -z $age || $age =~ [^0-9]+  || $age -ge 99 || $age -le 0 ]] && echo "Invalid age format" && continue
 	
 	read -e -p "Enter your user ID: " uid
-	[[ -z $uid || $uid -lt 0 || $uid == 0 || $uid -gt 999999 ]] && continue
+	[[ -z $uid || $uid =~ [^0-9]+ || $uid -lt 0 || $uid -gt 999999 ]] && echo " Invalid id format" && continue
 		
 	
 	echo "You are $forumname, aged $age , next year you will be $(($age+1)), with user id $uid,
